97351843a1eb7674639a9fd9d3e6d662d77141fb
famegfx <99344537+famegfx@users.noreply.github.com>
2022-07-13 12:13:16 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -63,7 +63,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}"
+                text=" sᴜᴩᴩᴏʀᴛ ", url=f"{config.SUPPORT_GROUP}"
             )
         ],
         [
